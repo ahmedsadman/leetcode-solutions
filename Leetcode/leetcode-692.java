c5133e93e4555e53e9ffe61f6d8eb4d8a09b4d79
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-05-07 00:43:05 +0600

    simplified solution for leetcode #692

--- Leetcode/leetcode-692.java
+++ Leetcode/leetcode-692.java
@@ -3,8 +3,11 @@ import java.util.*;
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> count = new HashMap<>();
+
+        // if counts match, we have to show in alphabetical order
         PriorityQueue<String> heap = new PriorityQueue<>(
-                (a, b) -> count.get(a).equals(count.get(b)) ? b.compareTo(a) : count.get(a) - count.get(b));
+                (a, b) -> count.get(a).equals(count.get(b)) ? a.compareTo(b) : count.get(b) - count.get(a));
+
         List<String> result = new ArrayList<>();
 
         for (String word : words) {
@@ -13,24 +16,18 @@ class Solution {
 
         for (String word : count.keySet()) {
             heap.offer(word);
-            // always keep the best k elements only
-            if (heap.size() > k)
-                heap.remove();
         }
 
-        while (!heap.isEmpty()) {
+        while (result.size() < k) {
             result.add(heap.remove());
         }
-
-        // reverse the array, to get the best ones first
-        Collections.reverse(result);
 
         return result;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String[] words = { "i", "love", "leetcode", "i", "love", "coding" };
-        sol.topKFrequent(words, 2);
+        String[] words = { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+        System.out.println(sol.topKFrequent(words, 4));
     }
 }
