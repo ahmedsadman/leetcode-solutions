bdecdeb8601b5e320479b501daf79144d19da885
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-05-17 23:55:15 +0600

    simplified leetcode #438

--- Leetcode/leetcode-438.java
+++ Leetcode/leetcode-438.java
@@ -1,38 +1,43 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        int[] hash = new int[256];
+
+        for (int i = 0; i < p.length(); i++)
+            hash[p.charAt(i)]++;
+
         int left = 0;
         int right = 0;
-        int[] hash = new int[256]; // to store letter count
+        int strLen = s.length();
         int count = p.length();
 
-        if (s == null || s.length() == 0)
-            return list;
-
-        // fill up the hash data
-        for (char c : p.toCharArray()) {
-            hash[c]++;
-        }
-
-        while (right < s.length()) {
-            // decrement the associated hash value as we move
-            // if an anagram letter is found, decrement count as well
-            if (hash[s.charAt(right++)]-- >= 1)
+        while (right < strLen) {
+            // if the character exists in p's hash, decrease the count
+            if (hash[s.charAt(right)] >= 1)
                 count--;
 
-            // if count gets to 0, it means we found an anagram permutation
-            if (count == 0)
-                list.add(left);
+            // decrease the used value and always move right
+            hash[s.charAt(right)]--;
+            right++;
 
-            // if anagram letter is not found within anagram length, narrow
-            // window by updating 'left' value and reset hash by increasing hash count
-            // Here the >=0 part ensures that we are dealing with a letter
-            // from the anagram, because the anagram letter count will never
-            // go below zero, as the working of the algorithm
-            if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0)
-                count++;
+            // we have found an answer
+            if (count == 0)
+                result.add(left);
+
+            // we narrow the window when window size is equals p
+            if (right - left == p.length()) {
+                // reset count and hash value
+                // NOTE: according to anagrams, containing character count will never reach
+                // below 0
+                if (hash[s.charAt(left)] >= 0)
+                    count++;
+                hash[s.charAt(left)]++;
+                left++;
+            }
         }
 
-        return list;
+        return result;
     }
 }
