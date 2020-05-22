12ed1dafb3432939676dad018c2675141bc8cf95
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-05-22 23:34:27 +0600

    simplified leetcode #567

--- Leetcode/leetcode-567.java
+++ Leetcode/leetcode-567.java
@@ -1,28 +1,41 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        /* Explanation is similar to Leetcode - 438 */
-        List<Integer> list = new ArrayList<>();
+        // same as leetcode 438
         int left = 0;
         int right = 0;
-        int[] hash = new int[256]; // to store letter count
         int count = s1.length();
-
-        // fill up the hash data
-        for (char c : s1.toCharArray()) {
-            hash[c]++;
-        }
-
-        while (right < s2.length()) {
-            if (hash[s2.charAt(right++)]-- >= 1)
+        int len = s2.length();
+        int[] map = new int[256];
+        
+        // fill the map
+        for (int i = 0; i < s1.length(); i++)
+            map[s1.charAt(i)]++;
+        
+        while (right < len) {
+            // if contains a character from s1, decrement count
+            if (map[s2.charAt(right)] >= 1)
                 count--;
-
+            
+            // decrase used value and move right
+            map[s2.charAt(right)]--;
+            right++;
+            
+            // check for answer
             if (count == 0)
                 return true;
-
-            if (right - left == s1.length() && hash[s2.charAt(left++)]++ >= 0)
-                count++;
+            
+            // if boundary is crossed, move left pointer
+            if (right - left == s1.length()) {
+                // restoration
+                if (map[s2.charAt(left)] >= 0)
+                    count++;
+                map[s2.charAt(left)]++;
+                
+                left++;
+            }
+            
         }
-
+        
         return false;
     }
 }
